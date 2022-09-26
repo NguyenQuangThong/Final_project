34fe9e6cbc31a76efc812abe8724670736a16944
nguyenquangthong <thongnguyen120600@gmail.com>
2022-09-26 11:27:27 +0700

    database design

--- src/main/java/com/example/final_project/models/File.java
+++ src/main/java/com/example/final_project/models/File.java
@@ -1,6 +1,7 @@
 package com.example.final_project.models;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
 public class File {
@@ -9,6 +10,8 @@ public class File {
     Long fileId;
     @Column(name = "file_path", nullable = false, columnDefinition = "varchar(100)")
     String filePath;
+    @Column(name = "uploaded_at", nullable = false, columnDefinition = "timestamp")
+    Timestamp timestamp;
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "classroom_id", nullable = false)
     Classroom classroom;
