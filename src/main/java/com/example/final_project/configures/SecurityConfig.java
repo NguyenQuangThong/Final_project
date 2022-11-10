9ccd22c11ad374ad54affc2c9ebfb68d3b5db762
nguyenquangthong <thongnguyen120600@gmail.com>
2022-11-10 10:10:56 +0700

    Adding heroku files

--- src/main/java/com/example/final_project/configures/SecurityConfig.java
+++ src/main/java/com/example/final_project/configures/SecurityConfig.java
@@ -31,8 +31,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
-        http
+//        http.cors().and().csrf().disable();
+        http.csrf().disable();
+        http.cors().and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().exceptionHandling()
