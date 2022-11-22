e739f24347ad7296ebbe243f6bcaf023f7b5e4da
nguyenquangthong <thongnguyen120600@gmail.com>
2022-11-22 10:28:43 +0700

    Refactor code v1

--- src/main/java/com/example/final_project/configures/SecurityConfig.java
+++ src/main/java/com/example/final_project/configures/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
