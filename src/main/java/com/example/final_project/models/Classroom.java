7f4747c5a96b6ef68a8151d51c832d3757b34a7b
nguyenquangthong <thongnguyen120600@gmail.com>
2022-11-30 11:30:01 +0700

    Add relationship between classroom and post

--- src/main/java/com/example/final_project/models/Classroom.java
+++ src/main/java/com/example/final_project/models/Classroom.java
@@ -13,7 +13,7 @@ public class Classroom {
     Long classroomId;
     @Column(name = "class_name", nullable = false, columnDefinition = "varchar(50)")
     String className;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "account_id", nullable = false)
     Account roomOwner;
     //    @OneToMany(fetch = FetchType.LAZY, mappedBy = "classroom", cascade = CascadeType.ALL)
